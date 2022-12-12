abec0311dbfc2ce556d101f9010ad24856f8cfcf
Yena Lee <lmonari71@gmail.com>
2022-12-12 23:36:43 +0900

    feat: sorting 함수 추가

--- Elice/basic/01_maxMachine.py
+++ Elice/basic/01_maxMachine.py
@@ -10,3 +10,27 @@ class maxMachine :
         self.numbers.remove(n)
     def getMax(self):
         return max(self.numbers)
+
+def sorting(myList) :
+    '''
+    myList를 내림차순으로 정렬하여 반환하는 함수를 작성하세요.
+
+    예를 들어, myList = [5, 2, 3, 1] 이라면 [5, 3, 2, 1] 을 반환해야 합니다.
+
+    단, maxMachine class를 이용하도록 합니다. 
+    '''
+
+    myMachine = maxMachine()
+    result = []
+
+    for x in myList:
+        myMachine.addNumber(x)
+    
+    for i in range(len(myList)) :
+        myMax = myMachine.getMax()
+
+        result.append(myMax)
+
+        myMachine.removeNumber(myMax)
+
+    return result
