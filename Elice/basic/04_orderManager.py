c996fcf1068e30e31abab783d818c89ee8579f19
Yena Lee <lmonari71@gmail.com>
2022-12-12 23:50:46 +0900

    feat: 주문 관리 시스템 (배열)

---
+++ Elice/basic/04_orderManager.py
@@ -0,0 +1,36 @@
+'''
+orderManager 클래스를 완성하세요.
+'''
+
+## 배열 사용
+
+class orderManager :
+    '''
+    주문을 처리하는 class를 작성합니다.
+    '''
+
+    def __init__(self) :
+        self.data = []
+
+    def addOrder(self, orderId) :
+        '''
+        주문번호 orderId를 추가합니다.
+        '''
+        self.data.append(orderId)
+
+    def removeOrder(self, orderId) :
+        '''
+        주문번호 orderId를 제거합니다.
+        '''
+        self.data.remove(orderId)
+
+    def getOrder(self, orderId) :
+        '''
+        주문번호 orderId가 몇 번째로 처리될지를 반환합니다.
+
+        만약 주문번호 orderId가 존재하지 않으면 -1을 반환합니다. 
+        '''
+        for i in range(len(self.data)) : 
+            if self.data[i] == orderId :
+                return (i + 1)
+        return -1
