7c392981f9d86afe1790a747c32229008a436805
Yena Lee <lmonari71@gmail.com>
2022-12-15 20:46:46 +0900

    add: js 폴더 추가

---
+++ javascript/divisor.js
@@ -0,0 +1,17 @@
+// 프로그래머스 Lv1.약수의합
+
+function solution(n) {
+  var answer = [];
+  var m = n;
+  var sum = 0;
+  while (m > 0) {
+    if (n % m == 0) {
+      answer.push(m);
+    }
+    m -= 1;
+  }
+  for (n of answer) {
+    sum += n;
+  }
+  return sum;
+}
